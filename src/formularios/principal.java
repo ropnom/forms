372369df6ab3d5e1c4faa66c4a8c5f5cfd797342
ropnom <rsampedr@I10020041.ad.intranet.local>
2015-07-28 12:11:18 +0200

    making first forms and propierties of component or planels

--- src/formularios/principal.java
+++ src/formularios/principal.java
@@ -1,5 +1,68 @@
 package formularios;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JToolBar;
+import java.awt.BorderLayout;
+import javax.swing.JProgressBar;
+import javax.swing.JButton;
+import javax.swing.JSeparator;
+import javax.swing.JDesktopPane;
+import javax.swing.JLabel;
+import java.awt.Dimension;
+
 public class principal {
 
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					principal window = new principal();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public principal() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 701, 620);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnfile = new JMenu("{File}");
+		menuBar.add(mnfile);
+		
+		JMenuItem mntmnew = new JMenuItem("{new}");
+		mnfile.add(mntmnew);
+		
+		JToolBar toolBar = new JToolBar();
+		frame.getContentPane().add(toolBar, BorderLayout.NORTH);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		frame.getContentPane().add(desktopPane, BorderLayout.CENTER);
+	}
 }
